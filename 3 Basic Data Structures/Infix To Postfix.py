60a1f1b5997e4910aec474e3337e531a05c7dd96
Zoe Oladokun <145172902+zozoking101@users.noreply.github.com>
2023-12-29 01:58:41 +0100

    Friday Codes Commit

--- 3 Basic Data Structures/Infix To Postfix.py
+++ 3 Basic Data Structures/Infix To Postfix.py
@@ -1,4 +1,4 @@
-from Stack import Stack # As previously defined
+from Stack import Stack
 
 def infix_to_postfix(infix_expr):
     prec = {}
@@ -12,7 +12,9 @@ def infix_to_postfix(infix_expr):
     token_list = infix_expr.split()
 
     for token in token_list:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+        if token.isnumeric():
+            postfix_list.append(token)
+        elif token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
             postfix_list.append(token)
         elif token == '(':
             op_stack.push(token)
@@ -22,13 +24,15 @@ def infix_to_postfix(infix_expr):
                 postfix_list.append(top_token)
                 top_token = op_stack.pop()
         else:
-            while (not op_stack.is_empty()) and (prec[op_stack.peek()] >= prec[token]):
+            while (not op_stack.is_empty()) and (op_stack.peek() != '(') and (prec.get(op_stack.peek(), 0) >= prec.get(token, 0)):
                 postfix_list.append(op_stack.pop())
             op_stack.push(token)
 
     while not op_stack.is_empty():
         postfix_list.append(op_stack.pop())
+    
     return " ".join(postfix_list)
 
 print(infix_to_postfix("A * B + C * D"))
 print(infix_to_postfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+print(infix_to_postfix("10 + 3 * 5 / (16 - 4)"))
